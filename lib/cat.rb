490c588c7d42476849802ef128ec744f8f760b67
KConlon13 <KyleConlon13@gmail.com>
2019-06-29 12:28:04 -0400

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,23 @@
 class Cat
   # code goes here
+  attr_accessor :mood, :owner
+  attr_writer
+  attr_reader :name
+  @@all = []
+
+    def initialize(name, mood) 
+      @name = name
+      @mood = "nervous"
+      Cat.all << self
+    end 
+
+    def self.all
+      @@all
+    end
+    
+    # def owner
+    #   Owner.all.select do |owner_instance|
+    #     owner_instance.cats
+    #   end
+    # end
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
   # code goes here
+  attr_accessor :mood, :owner
+  attr_writer
+  attr_reader :name
+  @@all = []
+
+    def initialize(name, mood)
+      @name = name
+      @mood = "nervous"
+      Dog.all << self
+    end
+
+    def self.all
+      @@all
+    end
 end
